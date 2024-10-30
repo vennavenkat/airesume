14bd7e6a740ee9cf57c178553eb942fbae70edbf
vennavenkat <vennavenkat1999@gmail.com>
2024-10-29 19:00:40 -0500

    lookup error

--- App/App.py
+++ App/App.py
@@ -42,15 +42,8 @@ import os
 # nltk.data.path.append('./nltk_data')
 import nltk
 import os
-
-# Ensure NLTK data goes to a directory that Streamlit can access
 nltk_data_path = os.path.join(os.getcwd(), "nltk_data")
-os.makedirs(nltk_data_path, exist_ok=True)
 nltk.data.path.append(nltk_data_path)
-
-# Download stopwords corpus to the specific path
-nltk.download('stopwords', download_dir=nltk_data_path)
-
 
 from pyresparser import ResumeParser
 
